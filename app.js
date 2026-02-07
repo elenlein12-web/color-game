5af6a79b06c94d6cd11a981cdced290109a2e0ce
Elene peikrishvili <elenlein12@gmail.com>
2026-02-07 16:33:57 +0400

    ch

--- app.js
+++ app.js
@@ -9,55 +9,11 @@ let score = 0;
 const easyBtn = document.querySelector(".easyBtn");
 const hardBtn = document.querySelector(".hardBtn");
 
-
 hardBtn.addEventListener("click", function () {
-function init() {
-  colorsDiv.innerHTML = "";
-  message.textContent = "";
-  const colors = [];
-
-  for (let i = 0; i < 12; i++) {
+  for (let i = 0; i < 6; i++) {
     colors.push(randomColor());
   }
-
-  correctColor = colors[Math.floor(Math.random() * colors.length)];
-
-  rgbDisplay.textContent = correctColor.toLocaleUpperCase();
-  colors.forEach((color) => {
-    const box = document.createElement("div");
-    box.classList.add("color-box");
-    box.style.backgroundColor = color;
-
-    box.addEventListener("click", () => {
-      if (color === correctColor) {
-        score += 5;
-        message.textContent = "პასუხი სწორია! +5 ქულა";
-        scoreDisplay.textContent = `ქულა : ${score}`;
-
-        resetBtn.textContent = "Play Again?";
-        document.body.style.backgroundColor = correctColor;
-      } else {
-        score -= 10;
-        scoreDisplay.textContent = `ქულა : ${score}`;
-        box.style.opacity = "0";
-        message.textContent = " სცადე ისევ!";
-      }
-    });
-    colorsDiv.appendChild(box);
-  });
-}
 });
-
-function updateGame(num) {
-  for (let i = 0; i < squares.length; i++) {
-    if (colors[i]) {
-      squares[i].style.display = "block";
-      squares[i].style.backgroundColor = colors[i];
-    } else {
-      squares[i].style.display = "none";
-    }
-  }
-}
 
 function randomColor() {
   const r = Math.floor(Math.random() * 256);
@@ -108,5 +64,3 @@ resetBtn.addEventListener("click", () => {
 });
 
 init();
-
-colors.Pushcolors.length;
